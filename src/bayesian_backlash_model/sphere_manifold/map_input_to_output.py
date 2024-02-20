16028601bebc2f2c5dbbdb321e39a3fd617b6ca0
OriginalPou <chaari.mahdi@yahoo.fr>
2024-02-20 10:41:31 +0100

    added angular velocity vector field in the ee

--- src/bayesian_backlash_model/sphere_manifold/map_input_to_output.py
+++ src/bayesian_backlash_model/sphere_manifold/map_input_to_output.py
@@ -1,8 +1,10 @@
 #%%
-from ukfm import SO3
+# from ukfm import SO3
 from backlash_simulation_examples.models.model_endoscope_two_bending_3D import EndoscopeTwoBendingModel
 
 import matplotlib.pyplot as plt
+from matplotlib.lines import Line2D
+
 import numpy as np
 from scipy.spatial.transform import Rotation as R
 
@@ -28,56 +30,57 @@ if __name__ == "__main__" :
     
     from fit_sphere_to_workspace import geodesic_distance
 
-    for i in range (q_var_q1.shape[0]):
-        new_Rcc = model.R_base_to_tip(np.array([q_var_q1[i], 0]))
-        angle_Z = - model.camera_model_params_["rotation_camera_wrt_bearing"]
-        mat_rotation_camera_wrt_bearing = np.array([
-            [np.cos(angle_Z), -np.sin(angle_Z), 0.0],
-            [np.sin(angle_Z),  np.cos(angle_Z), 0.0],
-            [0.0,              0.0,             1.0]
-        ])
+    # for i in range (q_var_q1.shape[0]):
+    #     new_Rcc = model.R_base_to_tip(np.array([q_var_q1[i], 0]))
+    #     angle_Z = - model.camera_model_params_["rotation_camera_wrt_bearing"]
+    #     mat_rotation_camera_wrt_bearing = np.array([
+    #         [np.cos(angle_Z), -np.sin(angle_Z), 0.0],
+    #         [np.sin(angle_Z),  np.cos(angle_Z), 0.0],
+    #         [0.0,              0.0,             1.0]
+    #     ])
         
         
         
-        tangent_vector = SO3.log(new_Rcc @ np.transpose(R_cc))
-        #print(tangent_vector)
+    #     tangent_vector = SO3.log(new_Rcc @ np.transpose(R_cc))
+    #     #print(tangent_vector)
         
-        angle = geodesic_distance(new_Rcc, R_cc)
-        #print(angle)
-        R_cc = new_Rcc
-        t_cc = model.t_base_to_tip(np.array([q_var_q1[i], 0]))
-        t_cc = (t_cc).reshape((-1,))
+    #     angle = geodesic_distance(new_Rcc, R_cc)
+    #     #print(angle)
+    #     R_cc = new_Rcc
+    #     t_cc = model.t_base_to_tip(np.array([q_var_q1[i], 0]))
+    #     t_cc = (t_cc).reshape((-1,))
 
-        Rot = Rot.dot(SO3.exp(np.array([tangent_vector[0],tangent_vector[1], 0])))
-        new_t = (Rot @ t)*radius + sphere_center
-        #print(np.linalg.norm(new_t-t_cc))
+    #     Rot = Rot.dot(SO3.exp(np.array([tangent_vector[0],tangent_vector[1], 0])))
+    #     new_t = (Rot @ t)*radius + sphere_center
+    #     #print(np.linalg.norm(new_t-t_cc))
 
     from plot_ref_frames import plot_frame
     from fit_sphere_to_workspace import draw_sphere
 
-    fig = plt.figure()
-    ax = fig.add_subplot(projection='3d')
-    ax.set_box_aspect([1,1,1])
-    ax.scatter(0,0,0, label="base")
+    # fig = plt.figure()
+    # ax = fig.add_subplot(projection='3d')
+    # ax.set_box_aspect([1,1,1])
+    # ax.scatter(0,0,0, label="base")
 
-    ax.set_xlabel('X')
-    ax.set_ylabel('Y')
-    ax.set_zlabel('Z')
+    # ax.set_xlabel('X')
+    # ax.set_ylabel('Y')
+    # ax.set_zlabel('Z')
 
-    draw_sphere(ax, sphere_center, radius)
+    # draw_sphere(ax, sphere_center, radius)
 
-    TR_44_Sphere = np.eye(4)
-    TR_44_Sphere[:-1, -1] = new_t.reshape((-1,))
-    TR_44_Sphere[:-1, :-1] = Rot
-    plot_frame(ax, TR_44 = TR_44_Sphere, frame_name = '', arrows_length = 0.05)
+    # TR_44_Sphere = np.eye(4)
+    # TR_44_Sphere[:-1, -1] = new_t.reshape((-1,))
+    # TR_44_Sphere[:-1, :-1] = Rot
+    # plot_frame(ax, TR_44 = TR_44_Sphere, frame_name = '', arrows_length = 0.05)
     
-    TR_44_Sphere = np.eye(4)
-    TR_44_Sphere[:-1, -1] = t_cc
-    TR_44_Sphere[:-1, :-1] = R_cc
-    plot_frame(ax, TR_44 = TR_44_Sphere, frame_name = '', arrows_length = 0.05)
-    ax.plot([0, t_cc[0]], [0, t_cc[1]], [0, t_cc[2]], '--')
+    # TR_44_Sphere = np.eye(4)
+    # TR_44_Sphere[:-1, -1] = t_cc
+    # TR_44_Sphere[:-1, :-1] = R_cc
+    # plot_frame(ax, TR_44 = TR_44_Sphere, frame_name = '', arrows_length = 0.05)
+    # ax.plot([0, t_cc[0]], [0, t_cc[1]], [0, t_cc[2]], '--')
 
-    ax.legend()
+    # ax.legend()
+    
     # plt.show()
 
     # p = np.zeros((q_var_q1.shape[0]*q_var_q2.shape[0],3))
@@ -92,28 +95,28 @@ if __name__ == "__main__" :
     ##              Compute the angle you move (or geodesic distance)
     ##              and the tangent vector to the SO(3) manifold
 
-    q1 = -actuator_limit/10
-    q2 = -actuator_limit/4
+    # q1 = -actuator_limit/10
+    # q2 = -actuator_limit/4
 
-    q_var = np.linspace(-actuator_limit/4, actuator_limit/4, 25)
+    # q_var = np.linspace(-actuator_limit/4, actuator_limit/4, 25)
 
     
-    q1_var = np.array([- actuator_limit/200, 0 , actuator_limit/200])
-    q2_var = np.array([- actuator_limit/200, 0 , actuator_limit/200])
+    # q1_var = np.array([- actuator_limit/200, 0 , actuator_limit/200])
+    # q2_var = np.array([- actuator_limit/200, 0 , actuator_limit/200])
 
-    for i in range(q_var.shape[0]-1):
-        Rot_2 = model.R_base_to_tip(np.array([actuator_limit/10, q_var[i]]))
-        Rot_dq2 = model.R_base_to_tip(np.array([actuator_limit/10, q_var[i+1]]))
+    # for i in range(q_var.shape[0]-1):
+    #     Rot_2 = model.R_base_to_tip(np.array([actuator_limit/10, q_var[i]]))
+    #     Rot_dq2 = model.R_base_to_tip(np.array([actuator_limit/10, q_var[i+1]]))
 
-        tangent_vector = SO3.log(np.transpose(Rot_2) @ Rot_dq2)
-        #print("the tangent vector for dq2 = %.5f and q2 = %.5f" %(q_var[i+1] - q_var[i], q_var[i]))
-        #print(tangent_vector)
+    #     tangent_vector = SO3.log(np.transpose(Rot_2) @ Rot_dq2)
+    #     #print("the tangent vector for dq2 = %.5f and q2 = %.5f" %(q_var[i+1] - q_var[i], q_var[i]))
+    #     #print(tangent_vector)
 
-        Rot_1 = model.R_base_to_tip(np.array([q_var[i], 0]))
-        Rot_dq1 = model.R_base_to_tip(np.array([q_var[i], actuator_limit/20]))
+    #     Rot_1 = model.R_base_to_tip(np.array([q_var[i], 0]))
+    #     Rot_dq1 = model.R_base_to_tip(np.array([q_var[i], actuator_limit/20]))
 
         
-        symmetry = geodesic_distance(Rot_dq2, Rot_2)-geodesic_distance(Rot_dq1, Rot_1)
+    #     symmetry = geodesic_distance(Rot_dq2, Rot_2)-geodesic_distance(Rot_dq1, Rot_1)
         #print("%.5f" % symmetry)
 
     # q1 = -actuator_limit/4
@@ -128,12 +131,12 @@ if __name__ == "__main__" :
     #     Rot = Rot_dq
 
     #%%    
-    ## Experiment : Sample points on the workspace and compute the 
-    ##              angular velocity jacobian w.r.t the cable actuation
-    ##              Compute the mean and std of the jacobians
+    # Experiment : Sample points on the workspace and compute the 
+    #              angular velocity jacobian w.r.t the cable actuation
+    #              Compute the mean and std of the jacobians
 
-    # q_var_q1 = np.linspace(- actuator_limit/4, actuator_limit/4, 10000)
-    # q_var_q2 = np.linspace(- actuator_limit/4, actuator_limit/4, 10000)
+    # q_var_q1 = np.linspace(- actuator_limit/4, actuator_limit/4, 1000)
+    # q_var_q2 = np.linspace(- actuator_limit/4, actuator_limit/4, 1000)
 
     # nb_samples = q_var_q1.shape[0]
 
@@ -144,10 +147,10 @@ if __name__ == "__main__" :
     #         ang_vel_jacobians_wrt_cables[:,:,i*nb_samples + j] = \
     #             model.jacobian_angular(np.array([q_var_q1[j], q_var_q2[i]]))
 
-    # print(np.mean(ang_vel_jacobians_wrt_cables, axis = 2))
-    # print(np.std(ang_vel_jacobians_wrt_cables, axis = 2, ddof = 1)) 
-    # print(np.max(ang_vel_jacobians_wrt_cables, axis = 2))  
-    # print(np.min(ang_vel_jacobians_wrt_cables, axis = 2))
+    #print(np.mean(ang_vel_jacobians_wrt_cables, axis = 2))
+    #print(np.std(ang_vel_jacobians_wrt_cables, axis = 2, ddof = 1)) 
+    #print(np.max(ang_vel_jacobians_wrt_cables, axis = 2))  
+    #print(np.min(ang_vel_jacobians_wrt_cables, axis = 2))
 
     """
     Mean = 
@@ -159,7 +162,43 @@ if __name__ == "__main__" :
     [[  5.50563895   4.92599069]
     [106.2251326  121.23648694]
     [ 36.02595182  36.02595182]]
-    """    
+    """   
+
+    #%%    
+    # Experiment : Sample points on the workspace and compute the 
+    #              angular velocity jacobian w.r.t the cable actuation in ee frame
+    #              Compute the mean and std of the jacobians
+
+    # q_var_q1 = np.linspace(- actuator_limit/4, actuator_limit/4, 1000)
+    # q_var_q2 = np.linspace(- actuator_limit/4, actuator_limit/4, 1000)
+
+    # nb_samples = q_var_q1.shape[0]
+
+    # ang_vel_jacobians_wrt_cables = np.zeros((3,2,nb_samples**2))
+
+    # for i in tqdm(range (nb_samples)):
+    #     for j in range(nb_samples):
+    #         ang_vel_jacobians_wrt_cables[:,:,i*nb_samples + j] = \
+    #             model.jacobian_angular_ee(np.array([q_var_q1[j], q_var_q2[i]]))
+
+    # print(np.mean(ang_vel_jacobians_wrt_cables, axis = 2))
+    # print(np.std(ang_vel_jacobians_wrt_cables, axis = 2, ddof = 1)) 
+    # #print(np.max(ang_vel_jacobians_wrt_cables, axis = 2))  
+    # #print(np.min(ang_vel_jacobians_wrt_cables, axis = 2))
+
+    """
+    Mean = 
+    [[ 2.22826202e-17  1.61106081e+02]
+    [-1.61106081e+02  9.54969437e-18]
+    [-1.09866960e-15  1.78806658e-15]]
+
+    std =
+    [[ 5.51519511  4.93453678]
+    [ 4.93453678  5.51519511]
+    [36.05531133 36.05531133]]
+    """   
+
+
 
     #%%
     ## Plot the angular velocity vector field
@@ -206,6 +245,12 @@ if __name__ == "__main__" :
     ax.set_xlabel('X')
     ax.set_ylabel('Y')
     ax.set_zlabel('Z')
+
+    colors = ['black', 'red']
+    lines = [Line2D([0], [0], color=c, linewidth=2, linestyle='-') for c in colors]
+    labels = ['ang vel', 'lin vel']
+    ax.legend(lines, labels)
+    ax.set_title("Angular velocity vector field old version")
        
 
     #%%
@@ -252,51 +297,54 @@ if __name__ == "__main__" :
     ax.set_xlabel('X')
     ax.set_ylabel('Y')
     ax.set_zlabel('Z')
-       
-    plt.show()
+    
+    colors = ['black', 'red']
+    lines = [Line2D([0], [0], color=c, linewidth=2, linestyle='-') for c in colors]
+    labels = ['ang vel', 'lin vel']
+    ax.legend(lines, labels)
+    ax.set_title("Angular velocity vector field in the end effector frame")
 
+    plt.show()
 
 # %%
     ## Experiment : compute the tangent vector of rotations
     ##              induced by small cable actuation
 
-q_var_q1 = np.linspace(- actuator_limit/4, actuator_limit/4, 1000)
-q_var_q2 = np.linspace(- actuator_limit/4, actuator_limit/4, 1000)
+    # q_var_q1 = np.linspace(- actuator_limit/4, actuator_limit/4, 1000)
+    # q_var_q2 = np.linspace(- actuator_limit/4, actuator_limit/4, 1000)
 
-nb_samples = q_var_q1.shape[0]
+    # nb_samples = q_var_q1.shape[0]
 
-ang_vel_jacobians_wrt_cables = np.zeros((3,1,nb_samples**2))
+    # ang_vel_jacobians_wrt_cables = np.zeros((3,1,nb_samples**2))
 
-for i in tqdm(range (nb_samples)):
-    for j in range(nb_samples):
-        Rot = model.R_base_to_tip(np.array([q_var_q1[j], q_var_q2[i]]))
-        Rot_dq = model.R_base_to_tip(np.array([q_var_q1[j] + 0.0001, q_var_q2[i]]))
+    # for i in tqdm(range (nb_samples)):
+    #     for j in range(nb_samples):
+    #         Rot = model.R_base_to_tip(np.array([q_var_q1[j], q_var_q2[i]]))
+    #         Rot_dq = model.R_base_to_tip(np.array([q_var_q1[j] + 0.0001, q_var_q2[i]]))
 
-        ang_vel_jacobians_wrt_cables[:,:,i*nb_samples + j] = SO3.log(Rot_dq @ np.transpose(Rot)).reshape((3,1))
-
-    
-
-
-
-
-
-# %%
-print(np.mean(ang_vel_jacobians_wrt_cables, axis = 2))
-print(np.std(ang_vel_jacobians_wrt_cables, axis = 2, ddof = 1))
-# %%
-ang_vel_jacobians_wrt_cables = np.zeros((3,1,nb_samples**2))
-
-for i in tqdm(range (nb_samples)):
-    for j in range(nb_samples):
-        Rot = model.R_base_to_tip(np.array([q_var_q1[j], q_var_q2[i]]))
-        Rot_dq = model.R_base_to_tip(np.array([q_var_q1[j] , q_var_q2[i]+ 0.001]))
-
-        ang_vel_jacobians_wrt_cables[:,:,i*nb_samples + j] = SO3.log(Rot_dq @ np.transpose(Rot)).reshape((3,1))
+    #         ang_vel_jacobians_wrt_cables[:,:,i*nb_samples + j] = SO3.log(Rot_dq @ np.transpose(Rot)).reshape((3,1))
 
     
 
-# %%
-print(np.mean(ang_vel_jacobians_wrt_cables, axis = 2))
-print(np.std(ang_vel_jacobians_wrt_cables, axis = 2, ddof = 1))
 
-# %%
+
+
+
+    # # %%
+    # print(np.mean(ang_vel_jacobians_wrt_cables, axis = 2))
+    # print(np.std(ang_vel_jacobians_wrt_cables, axis = 2, ddof = 1))
+    # # %%
+    # ang_vel_jacobians_wrt_cables = np.zeros((3,1,nb_samples**2))
+
+    # for i in tqdm(range (nb_samples)):
+    #     for j in range(nb_samples):
+    #         Rot = model.R_base_to_tip(np.array([q_var_q1[j], q_var_q2[i]]))
+    #         Rot_dq = model.R_base_to_tip(np.array([q_var_q1[j] , q_var_q2[i]+ 0.001]))
+
+    #         ang_vel_jacobians_wrt_cables[:,:,i*nb_samples + j] = SO3.log(Rot_dq @ np.transpose(Rot)).reshape((3,1))
+
+        
+
+    # # %%
+    # print(np.mean(ang_vel_jacobians_wrt_cables, axis = 2))
+    # print(np.std(ang_vel_jacobians_wrt_cables, axis = 2, ddof = 1))
